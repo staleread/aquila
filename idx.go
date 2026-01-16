90df6abb4e4493ce1b679f0fe2a31cd61290f212
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2026-01-16 13:31:10 +0200

    Remove unused func

--- idx.go
+++ idx.go
@@ -5,17 +5,3 @@ type idx uint16
 func randIdx() idx {
 	return idx(randByte()) | idx(randByte())<<8
 }
-
-func readRandUniqueIds(ids []idx, maxIdx idx) {
-	n := len(ids)
-	added := make(map[idx]struct{})
-
-	for len(added) < n {
-		id := randIdx() % maxIdx
-
-		if _, ok := added[id]; !ok {
-			ids[len(added)] = id
-			added[id] = struct{}{}
-		}
-	}
-}
