8327a7d3c6cd4a3a4d09935698bf45bd9c869419
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2026-02-16 10:19:54 +0200

    Decrease the size of a subscript

--- internal/gf2/sym.go
+++ internal/gf2/sym.go
@@ -2,16 +2,16 @@ package gf2
 
 import "crypto/rand"
 
-type Sym uint16
+type Sym uint8
 
 func RandSyms(n int) []Sym {
 	syms := make([]Sym, n)
 
-	buf := make([]byte, n*2)
+	buf := make([]byte, n)
 	rand.Read(buf)
 
 	for i := range n {
-		syms[i] = (Sym(buf[i]) << 8) | Sym(buf[n+i])
+		syms[i] = Sym(buf[i])
 	}
 	return syms
 }
