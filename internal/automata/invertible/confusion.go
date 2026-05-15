962fc319c9c6fef01fcc2f91a90af9b8eb5eebc5
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2026-05-15 10:39:04 +0300

    Implement confusion map

---
+++ internal/automata/invertible/confusion.go
@@ -0,0 +1,57 @@
+package invertible
+
+import (
+	"github.com/staleread/aquila/internal/automata"
+	"slices"
+)
+
+type ConfusionMap []Subscript
+
+func (p ConfusionMap) FillRand(maxSub Subscript) {
+	FillRandSubscripts(p, maxSub)
+
+	subIdx := 0
+	for range automata.FoldSize {
+		for j := automata.ConfusionMapDegree; j > 0; j-- {
+			upperBound := maxSub - Subscript(j)
+
+			for k := range j {
+				candidate := p[subIdx] % (upperBound + Subscript(k) + 1)
+				duplicate := slices.Contains(p[subIdx-k:subIdx], candidate)
+
+				if duplicate {
+					p[subIdx] = upperBound + Subscript(k)
+				} else {
+					p[subIdx] = candidate
+				}
+				subIdx++
+			}
+		}
+	}
+}
+
+func (p ConfusionMap) Eval(state []uint64) Vector {
+	var res Vector
+	subIdx := 0
+
+	for i := range 16 {
+		var sum Vector
+
+		for j := range automata.ConfusionMapDegree {
+			prod := Ones
+			subCnt := automata.ConfusionMapDegree - j
+
+			for range subCnt {
+				idx := uint8(p[subIdx])
+
+				bit := (state[idx/64] >> (idx % 64)) & 1
+				prod &= Vector(bit)
+
+				subIdx++
+			}
+			sum ^= prod
+		}
+		res |= sum << i
+	}
+	return res
+}
