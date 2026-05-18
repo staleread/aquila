7fdae730e3da8c1c0158b7d23212123390caf8ce
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2026-05-19 01:08:07 +0300

    Add derivation test

---
+++ internal/automata/invertible/derivation_test.go
@@ -0,0 +1,39 @@
+package invertible_test
+
+import (
+	"bytes"
+	"math/rand"
+	"testing"
+
+	"github.com/staleread/aquila/internal/automata/core"
+	"github.com/staleread/aquila/internal/automata/invertible"
+)
+
+func TestDeriveGeneralCA(t *testing.T) {
+	ca := invertible.NewCA()
+	rng := rand.New(rand.NewSource(42))
+
+	if err := ca.Generate(rng); err != nil {
+		t.Fatalf("Failed to generate CA: %v", err)
+	}
+
+	gen, err := ca.DeriveGeneralCA()
+	if err != nil {
+		t.Fatalf("Failed to derive general CA: %v", err)
+	}
+
+	src := make([]byte, core.BlockBytes)
+	dstInvertible := make([]byte, core.BlockBytes)
+	dstGeneral := make([]byte, core.BlockBytes)
+
+	for i := range 5 {
+		rng.Read(src)
+
+		ca.Apply(dstInvertible, src)
+		gen.Apply(dstGeneral, src)
+
+		if !bytes.Equal(dstInvertible, dstGeneral) {
+			t.Errorf("Inconsistency found at iteration %d:\nInvertible Apply: %x\nGeneral Apply:    %x", i, dstInvertible, dstGeneral)
+		}
+	}
+}
